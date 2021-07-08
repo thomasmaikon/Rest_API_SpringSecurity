f19d6f039a0ef54e0ae65c82eedcb4b336f6d58c
Thomas Maikon <44036917+thomasmaikon@users.noreply.github.com>
2021-07-08 11:43:23 -0300

    Delete java/com/example/jwt directory

--- java/com/example/jwt/DB/UsinaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jwt.DB;
-
-import com.example.jwt.model.UsinaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsinaRepository extends JpaRepository<UsinaModel, String> {
-}
--- java/com/example/jwt/DB/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jwt.DB;
-
-import com.example.jwt.model.UsuarioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, String> {
-
-}
